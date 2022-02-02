668263035493273a7ac1a1fcf790627012e52e76
Christian Castillo <castillochristian3@gmail.com>
2022-02-02 17:12:59 -0500

    commit tree 6e5ce7b596ffa3f441aa8c451fdc0fd1cfe8730b

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 10
 
-// Write your code here
+const num2 = parseFloat('6.2')
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()) * 10;
+
+const num3 = 104
+
+const num4 = 20
+
+const mod = 104 % 20;
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
+
